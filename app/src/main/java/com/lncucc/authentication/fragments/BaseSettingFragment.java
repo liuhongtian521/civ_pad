b1582dcafbe0a9e03d6cae687fb247c6dc6dc773
lisp105 <lisp105@chinaunicom.cn>
2021-08-04 18:50:01 +0800

    feat: 声音控制部分调试

--- app/src/main/java/com/lncucc/authentication/fragments/BaseSettingFragment.java
+++ app/src/main/java/com/lncucc/authentication/fragments/BaseSettingFragment.java
@@ -8,6 +8,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 
+import com.blankj.utilcode.util.LogUtils;
+import com.qmuiteam.qmui.widget.QMUISlider;
+
 import com.askia.common.base.BaseActivity;
 import com.askia.common.base.BaseFragment;
 import com.lncucc.authentication.R;
@@ -18,14 +21,47 @@ import org.jetbrains.annotations.NotNull;
 /**
  * 数据导入
  */
-public class BaseSettingFragment extends BaseFragment {
+public class BaseSettingFragment extends BaseFragment  {
     private FragmentBaseSettingBinding baseSetting;
     private AudioManager mgr;
+    QMUISlider mSlider;
 
     @Override
     public void onInit() {
         mgr = (AudioManager) mActivity.getSystemService(mActivity.AUDIO_SERVICE);
-        adjustStreamVolume();
+        mSlider = baseSetting.slider;
+        mSlider.setCurrentProgress(100);
+        mSlider.setCallback(new QMUISlider.Callback(){
+
+            @Override
+            public void onProgressChange(QMUISlider slider, int progress, int tickCount, boolean fromUser) {
+                LogUtils.e("change",progress);
+
+            }
+
+            @Override
+            public void onTouchDown(QMUISlider slider, int progress, int tickCount, boolean hitThumb) {
+                LogUtils.e("11",progress);
+            }
+
+            @Override
+            public void onTouchUp(QMUISlider slider, int progress, int tickCount) {
+                // slider 滑动后设置声音
+                mgr.setStreamVolume(AudioManager.STREAM_SYSTEM, progress * mgr.getStreamMaxVolume(AudioManager.STREAM_SYSTEM) / 100, AudioManager.FLAG_PLAY_SOUND);
+
+
+            }
+
+            @Override
+            public void onStartMoving(QMUISlider slider, int progress, int tickCount) {
+
+            }
+
+            @Override
+            public void onStopMoving(QMUISlider slider, int progress, int tickCount) {
+
+            }
+        });
     }
 
     @Override
@@ -44,10 +80,9 @@ public class BaseSettingFragment extends BaseFragment {
 
     }
 
-    public void adjustStreamVolume () {
-        mgr.setStreamVolume(AudioManager.STREAM_SYSTEM, mgr.getStreamMaxVolume(AudioManager.STREAM_SYSTEM), AudioManager.FLAG_SHOW_UI);
-
+    public void onProgressChange(QMUISlider slider, int progress, int tickCount, boolean fromUser) {
 
     }
+
 
 }
