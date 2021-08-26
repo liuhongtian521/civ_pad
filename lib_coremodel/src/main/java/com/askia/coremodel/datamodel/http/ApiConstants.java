92db5d1ef1ac36e95636e441ac618f081b76d172
v_yangmanyu <yangmanyu@neusoft.com>
2021-08-26 09:19:18 +0800

    修正HOST地址

--- lib_coremodel/src/main/java/com/askia/coremodel/datamodel/http/ApiConstants.java
+++ lib_coremodel/src/main/java/com/askia/coremodel/datamodel/http/ApiConstants.java
@@ -3,7 +3,7 @@ package com.askia.coremodel.datamodel.http;
 
 public class ApiConstants {
      //192.168.1.131
-     public static final String HOST = "http://192.168.1.131:8080";//本机测试
-//     public static final String HOST = "http://192.168.1.212:8001"; // 测试环境
+//     public static final String HOST = "http://192.168.1.131:8080";//本机测试
+     public static final String HOST = "http://192.168.1.212:8001"; // 测试环境
 
 }
